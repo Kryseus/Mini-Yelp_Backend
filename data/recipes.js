ed98d0298668f5a91c73478c65750b2fe0f01c3a
Simon <vonsimon2@gmail.com>
2021-07-26 15:33:16 +0200

    GET request working

---
+++ data/recipes.js
@@ -0,0 +1,22 @@
+let recipes = [
+    {
+    id: 1,
+    name: 'Spagetthi',
+    Description: 'noodles',
+    Ingredients: 'Pasta',
+    Photo: 'link1',
+    Instructions: 'Boil water',
+    Datum: '02.07.2021'
+    },
+    {
+    id: 2,
+    name: 'Hamburger',
+    Description: 'meat sandwich',
+    Ingredients: 'beef',
+    Photo: 'link2',
+    Instructions: 'Cook patty',
+    Datum: '03.07.2021'
+     },
+];
+
+export default recipes
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import recipes from './data/recipes.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get('/api/recipes', (req, res) => res.json(recipes));
+app.get('/', (req, res) => res.send("Hello World"));
+
 
 app.listen(port, () => console.log(`Servers listening at http://localhost:${port}`));
