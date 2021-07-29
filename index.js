55c66dcf22e466ed03c3fa88e2608891922fc4ec
Philip Tronser <tronser@web.de>
2021-07-29 13:57:29 +0200

    .env shit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+import 'dotenv/config.js';
 import express from 'express';
 import recipeRouter from './routes/recipesRouter.js';
+config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+console.log(pkg)
 
 app.use(express.json());
 app.use('/api/recipes', recipeRouter);
